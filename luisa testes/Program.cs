b37cc7941ba0746f9c6ae629b443a4673fdedb5a
Luísa Lima <luisalima0602@gmail.com>
2022-02-23 22:15:47 +0000

    Testando..

--- luisa testes/Program.cs
+++ luisa testes/Program.cs
@@ -10,7 +10,6 @@ class Program{
         case 2 : MenuAdm();break;
       }
     }while(opcao != 0);
-
 }
   public static int Menu(){
     Console.WriteLine();
@@ -26,18 +25,29 @@ class Program{
   }
   public static int MenuCliente(){
     Console.WriteLine();
-    Console.WriteLine("------ Escolha uma opção ------");
+    Console.WriteLine("- Bem-Vindo(a)! Escolha uma opção -");
     Console.WriteLine("01 - Listar produtos.");
     Console.WriteLine("02 - Adicionar Item ao carrinho.");
     Console.WriteLine("03 - Abrir carrinho.");
     Console.WriteLine("00 - Finalizar programa.");
-    Console.WriteLine("-------------------------------");
+    Console.WriteLine("-----------------------------------");
     Console.Write("Opção: ");
     int opcao = int.Parse(Console.ReadLine());
     Console.WriteLine();
     return opcao;
   }
   public static int MenuAdm(){
-    return 0;
+    Console.WriteLine();
+    Console.WriteLine("- Bem-Vindo(a)! Escolha uma opção -");
+    Console.WriteLine("01 - Listar produtos.");
+    Console.WriteLine("02 - Adicionar produto");
+    Console.WriteLine("03 - Excluir produto");
+    Console.WriteLine("03 - Editar produto");
+    Console.WriteLine("00 - Finalizar programa.");
+    Console.WriteLine("-----------------------------------");
+    Console.Write("Opção: ");
+    int opcao = int.Parse(Console.ReadLine());
+    Console.WriteLine();
+    return opcao;
   }
 }
